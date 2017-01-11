40bf11ff688308b771780e698ed32eba8ec46b67
Eric Quackenbush <equack@gmail.com>
2017-01-11 09:24:01 -0500

    change weak_peaks.m to function; added test file

---
+++ MATLAB/test_weak_peaks.m
@@ -0,0 +1,34 @@
+clear;
+clc;
+
+img = imread('test.tiff');
+
+% imag properties
+h = size(img, 1);
+w = size(img, 2);
+
+% generate random points
+numPts = 200;
+x=rand(1,numPts)*w;
+y=rand(1,numPts)*h;
+
+avg = zeros(numPts,1);
+sd = zeros(numPts,1);
+
+for i = 1:numPts
+    [xval, yval] = meshgrid(1:w, 1:h);
+    mask = ((xval-x(i)).^2 + (yval-y(i)).^2) <= 10;
+    values = img(mask);
+
+    avg(i) = mean(values);
+    sd(i) = std(double(values));
+end
+
+[minI, ind] = min(avg);
+minI = (minI - 3*sd(ind))/65535;
+
+[maxI, ind] = max(avg);
+maxI = (maxI + 5*sd(ind))/65535;
+
+K = imadjust(img,[minI maxI],[]);
+imagesc(K);
--- MATLAB/weak_peaks.m
+++ MATLAB/weak_peaks.m
@@ -1,36 +1,32 @@
 % Replicates the Fit2D Weak Peaks function
+function [K] = weak_peaks(img)
 
-clear;
-clc;
+    % imag properties
+    h = size(img, 1);
+    w = size(img, 2);
 
-% load image
-img = imread('test.tiff');
+    % generate random points
+    numPts = 200;
+    x=rand(1,numPts)*w;
+    y=rand(1,numPts)*h;
 
-h = size(img, 1);
-w = size(img, 2);
+    avg = zeros(numPts,1);
+    sd = zeros(numPts,1);
 
-% generate random points
-numPts = 200;
-x=rand(1,numPts)*w;
-y=rand(1,numPts)*h;
+    for i = 1:numPts
+        [xval, yval] = meshgrid(1:w, 1:h);
+        mask = ((xval-x(i)).^2 + (yval-y(i)).^2) <= 10;
+        values = img(mask);
 
-avg = zeros(numPts,1);
-sd = zeros(numPts,1);
+        avg(i) = mean(values);
+        sd(i) = std(double(values));
+    end
 
-for i = 1:numPts
-    [xval, yval] = meshgrid(1:w, 1:h);
-    mask = ((xval-x(i)).^2 + (yval-y(i)).^2) <= 10;
-    values = img(mask);
+    [minI, ind] = min(avg);
+    minI = (minI - 3*sd(ind))/65535;
 
-    avg(i) = mean(values);
-    sd(i) = std(double(values));
+    [maxI, ind] = max(avg);
+    maxI = (maxI + 5*sd(ind))/65535;
+
+    K = imadjust(img,[minI maxI],[]);
 end
-
-[minI, ind] = min(avg);
-minI = (minI - 3*sd(ind))/65535;
-
-[maxI, ind] = max(avg);
-maxI = (maxI + 5*sd(ind))/65535;
-
-K = imadjust(img,[minI maxI],[]);
-imagesc(K);
