a16e4152f6a06ea646f93a53704baa3d8c54dce7
Eric Quackenbush <equack@gmail.com>
2017-01-13 16:34:46 -0500

    cleaned up code

--- MATLAB/test_is_degenerate.m
+++ MATLAB/test_is_degenerate.m
@@ -2,14 +2,10 @@
 
 a = -100;
 b = 100;
-%value = 0;
+value = 0;
 
-% while (value == 0)
-%     M = (b-a)*rand(5,2) + a;
-%     [A, B, C, D, E, F] = conic_coeffs(M);
-%     [value, detM] = is_degenerate(A, B, C, D, E, F);
-% end
-
-M = [1, 5; 0, 9; 3, 13; 1, 1; 5, 26];
-[A, B, C, D, E, F] = conic_coeffs(M);
-[value] = is_degenerate(A, B, C, D, E, F);
+while (value == 0)
+    M = (b-a)*rand(5,2) + a;
+    [A, B, C, D, E, F] = conic_coeffs(M);
+    [value, detM] = is_degenerate(A, B, C, D, E, F);
+end
