f347edc0d7e8b8283449350c965ebe1b773dc388
Eric Quackenbush <equack@gmail.com>
2017-01-13 09:59:09 -0500

    added file to convert rectangular images to polar

---
+++ MATLAB/topolar.m
@@ -0,0 +1,29 @@
+clear;
+clear all;
+clc;
+
+M=fspecial('gaussian',256,32); % generate fake image
+ X0=size(M,1)/2; Y0=size(M,2)/2;
+ [Y X z]=find(M);
+ X=X-X0; Y=Y-Y0;
+ theta = atan2(Y,X);
+ rho = sqrt(X.^2+Y.^2);
+
+ % Determine the minimum and the maximum x and y values:
+ rmin = min(rho); tmin = min(theta);
+ rmax = max(rho); tmax = max(theta);
+
+ % Define the resolution of the grid:
+ rres=128; % # of grid points for R coordinate. (change to needed binning)
+ tres=128; % # of grid points for theta coordinate (change to needed binning)
+
+ F = TriScatteredInterp(rho,theta,z,'natural');
+
+ %Evaluate the interpolant at the locations (rhoi, thetai).
+ %The corresponding value at these locations is Zinterp:
+
+ [rhoi,thetai] = meshgrid(linspace(rmin,rmax,rres),linspace(tmin,tmax,tres));
+ Zinterp = F(rhoi,thetai);
+
+ subplot(1,2,1); imagesc(M) ; axis square
+ subplot(1,2,2); imagesc(Zinterp) ; axis square
