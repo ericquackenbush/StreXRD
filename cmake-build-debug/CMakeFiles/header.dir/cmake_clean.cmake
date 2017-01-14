a2c75c4d9e43d000b6d7e60ed5579beb25b68b98
Eric Quackenbush <equack@gmail.com>
2017-01-14 06:00:41 -0500

    organized files

---
+++ cmake-build-debug/CMakeFiles/header.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/header.dir/src/io/header.cpp.o"
+  "../bin/header.pdb"
+  "../bin/header"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/header.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sandbox/gui/HelloWorld/HelloWorld.pro
@@ -0,0 +1,3 @@
+TEMPLATE += app
+QT += core gui wigets
+SOURCES += main.cpp
